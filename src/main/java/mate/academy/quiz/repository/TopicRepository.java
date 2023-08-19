3bea1a5a9f0bf667088b275a9c8722a4cce30848
Anastasiia Variichuk <stacyvariuchuk@gmail.com>
2023-08-19 20:50:52 +0300

    fix TopicRepository

--- src/main/java/mate/academy/quiz/repository/TopicRepository.java
+++ src/main/java/mate/academy/quiz/repository/TopicRepository.java
@@ -1,13 +1,13 @@
 package mate.academy.quiz.repository;
 
 import mate.academy.quiz.model.Topic;
-import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
-public interface TopicRepository  extends JpaRepository<Topic,Long> {
+public interface TopicRepository extends CrudRepository<Topic,Long> {
     @Query("SELECT CASE WHEN COUNT(t) > 0 THEN true ELSE false " +
             "END FROM Topic t WHERE t.name = :name")
     boolean isTopicExistByName(@Param("name") String name);
